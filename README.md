23040e92ead2113972caa931e2a899c4419be239
Greg Willard (r3pwn) <Wickett06@gmail.com>
2020-04-04 23:21:20 -0400

    [exif] adapt exif module to use PIL
    this removes our dependency on the 'exif' CLI utility and allows it to work across multiple platforms

--- README.md
+++ README.md
@@ -5,10 +5,6 @@ Be sure that `python3` and `python3-pip` are installed:
 
 `sudo apt install python3 python3-pip`
 
-Now we have to make sure that any other dependencies are installed:
-
-`sudo apt install exif`
-
 ## Starting the tool
 We can now start the utility by running:
 
--- addons/exif.py
+++ addons/exif.py
@@ -1,27 +1,37 @@
 #!/usr/bin/env python3
-import subprocess
+import piexif
+from PIL import Image, ExifTags
 import sys
 import argparse
 
 parser = argparse.ArgumentParser(prog='{} exif'.format(sys.argv[0]), 
             description='view/edit the exif data of images', add_help=False)
+parser.add_argument('string', metavar='image', type=str,
+                    help='the path to an image')
 
 def main(args):
-    # tool exif 'filename'
-    process = subprocess.Popen(['exif', args[2]], 
-                           stdout=subprocess.PIPE,
-                           universal_newlines=True)
+    print("Image:", args[2])
+    try:
+        img = Image.open(args[2])
+        exif_dict = piexif.load(img.info["exif"])
+    except:
+        print("Not a supported image type or no metadata found")
+        return
 
-    while True:
-        output = process.stdout.readline()
-        print(output.strip())
-        # Do something else
-        return_code = process.poll()
-        if return_code is not None:
-            # Process has finished, read rest of the output 
-            for output in process.stdout.readlines():
-                print(output.strip())
-            break
+    if exif_dict:
+        # specify the metadata types to return
+        metadatatypes = ['0th', 'Exif', 'GPS', 'Interop', '1st']
+
+        for metadatatype in metadatatypes:
+            print("Section:", metadatatype)
+            for key, val in exif_dict[metadatatype].items():
+                if key in ExifTags.TAGS:
+                    print(f"  {ExifTags.TAGS[key]}: {repr(val)}")
+            print("----------------------------------------")
+    else:
+        # no metadata found in image
+        print("No metadata found in image")
+        return
 
 def short_desc():
     print("  exif - " + parser.description)
